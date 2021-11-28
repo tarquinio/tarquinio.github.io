ee4a210badf1b9ed58c850d9c7f8a4a8e64bbae2
Agustin Castiarena <agustin.castiarena@realworld-one.com>
2021-11-28 12:32:27 -0300

    try pre build commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import { ChakraProvider, Heading } from '@chakra-ui/react'
+import { ChakraProvider, Heading, Text } from '@chakra-ui/react'
 import { theme } from './theme'
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Heading>Tarquinio`s website</Heading>
+    <Text>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam at,
+      consequatur facilis nesciunt non pariatur porro sint sunt velit veritatis.
+      Dolorum exercitationem fugiat illo libero minus necessitatibus sequi,
+      suscipit velit!
+    </Text>
   </ChakraProvider>
 )
