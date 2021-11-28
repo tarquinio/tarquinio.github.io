64d75854cb93bba2ed6e83d35c60520005f5157f
Agustin Castiarena <agustin.castiarena@realworld-one.com>
2021-11-28 11:26:46 -0300

    change title

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { ChakraProvider, Input, Heading } from '@chakra-ui/react'
+import { ChakraProvider, Heading } from '@chakra-ui/react'
 import { theme } from './theme'
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Input background="gray.100" />
-    <Heading>Hello from react</Heading>
+    <Heading>Tarquinio`s website</Heading>
   </ChakraProvider>
 )
