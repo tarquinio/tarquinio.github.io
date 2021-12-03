9fb88104fc33cf7a84b56bcc540e83ce6ce8b22d
Agustin Castiarena <castiarena@gmail.com>
2021-12-02 21:00:01 -0300

    firebase connected

---
+++ src/data/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getDatabase } from 'firebase/database'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyAuIoRIQDFc68FAoPsTSVLyndVvAAM2qTo',
+  authDomain: 'codersouth-rtc.firebaseapp.com',
+  projectId: 'codersouth-rtc',
+  storageBucket: 'codersouth-rtc.appspot.com',
+  messagingSenderId: '208542950495',
+  appId: '1:208542950495:web:c674aeb1944e23cb3967ea',
+  measurementId: 'G-926R3L32MN',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+export const db = getDatabase(app)
--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -1,7 +1,12 @@
-import React, { VFC } from 'react'
+import React, { useEffect, VFC } from 'react'
 import { Container, Text } from '@chakra-ui/react'
+import { db } from '../data/firebase'
 
 export const Intro: VFC = () => {
+  useEffect(() => {
+    console.log(db)
+  })
+
   return (
     <Container maxH="container.lg">
       <Text>Hello world</Text>
